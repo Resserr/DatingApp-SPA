3f3b72c8f63e036eb193d7b2bf3eeb8a0b5452cd
Rostyslav Ostapiuk <rostyslav.ostapiuk@gmail.com>
2019-03-11 00:16:39 +0100

    add auth service

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  mainUrl: any = 'http://localhost:5000/api/auth/';
+  constructor(private httpClient: HttpClient) {}
+
+  // method responsible for login operation with returning user token
+  logIn(user: any): Observable<void> {
+    return this.httpClient.post(this.mainUrl + 'login', user)
+    .pipe(
+      map( (data: any) => {
+        if (data) {
+          localStorage.setItem('token', data.generatedToken);
+        }
+      })
+    );
+  }
+}

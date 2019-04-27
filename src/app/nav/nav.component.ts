e8f23486c4682e91bf63c3805c290b93af4e7217
Rostyslav Ostapiuk <rostyslav.ostapiuk@gmail.com>
2019-04-27 23:21:00 +0200

    add new components

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,7 @@ export class NavComponent implements OnInit {
   model: any = {};
   jwtDecoder: JwtHelperService = new JwtHelperService();
 
-  constructor(public serviceAuth: AuthService, private alertifyService: AlertifyService) { }
+  constructor(public serviceAuth: AuthService, private alertifyService: AlertifyService, private router: Router) { }
 
   ngOnInit() {
     const token: string = localStorage.getItem('token');
@@ -23,7 +24,8 @@ export class NavComponent implements OnInit {
 
   logIn(): void {
     this.serviceAuth.logIn(this.model).subscribe(() => this.alertifyService.success('Successfully loged in: ' + this.model),
-    error => this.alertifyService.error(error));
+    error => this.alertifyService.error(error),
+    () => this.router.navigate(['']));
   }
 
   logedIn(): boolean {
@@ -33,6 +35,7 @@ export class NavComponent implements OnInit {
   logOut(): void {
     localStorage.removeItem('token');
     this.alertifyService.success('Successfully loged out.');
+    this.router.navigate(['/home']);
   }
 
 }
